bc62f9db423260e80079244eecc86bcb67dca35f
Oscar Estrada <arturostrada2020@gmail.com>
2022-09-02 23:53:08 -0500

    connect endpoints

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+const api_base = 'http://localhost:3000';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [todos, setTodos] = useState([]);
+	const [popupActive, setPopupActive] = useState(false);
+	const [newTodo, setNewTodo] = useState("");
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	useEffect(() => {
+		GetTodos();
+	}, []);
+
+	const GetTodos = () => {
+		fetch(api_base + '/todo')
+			.then(res => res.json())
+			.then(data => setTodos(data))
+			.catch((err) => console.error("Error: ", err));
+	}
+
+	const completeTodo = async id => {
+		const data = await fetch(api_base + '/todo/complete/' + id).then(res => res.json());
+
+		setTodos(todos => todos.map(todo => {
+			if (todo._id === data._id) {
+				todo.complete = data.complete;
+			}
+
+			return todo;
+		}));
+		
+	}
+
+	const addTodo = async () => {
+		const data = await fetch(api_base + "/todo/new", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json" 
+			},
+			body: JSON.stringify({
+				text: newTodo
+			})
+		}).then(res => res.json());
+
+		setTodos([...todos, data]);
+
+		setPopupActive(false);
+		setNewTodo("");
+	}
+
+	const deleteTodo = async id => {
+		const data = await fetch(api_base + '/todo/delete/' + id, { method: "DELETE" }).then(res => res.json());
+
+		setTodos(todos => todos.filter(todo => todo._id !== data.result._id));
+	}
+
+	return (
+		<div className="App">
+			<h1>Welcome, Tyler</h1>
+			<h4>Your tasks</h4>
+
+			<div className="todos">
+				{todos.length > 0 ? todos.map(todo => (
+					<div className={
+						"todo" + (todo.complete ? " is-complete" : "")
+					} key={todo._id} onClick={() => completeTodo(todo._id)}>
+						<div className="checkbox"></div>
+
+						<div className="text">{todo.text}</div>
+
+						<div className="delete-todo" onClick={() => deleteTodo(todo._id)}>x</div>
+					</div>
+				)) : (
+					<p>You currently have no tasks</p>
+				)}
+			</div>
+
+			<div className="addPopup" onClick={() => setPopupActive(true)}>+</div>
+
+			{popupActive ? (
+				<div className="popup">
+					<div className="closePopup" onClick={() => setPopupActive(false)}>X</div>
+					<div className="content">
+						<h3>Add Task</h3>
+						<input type="text" className="add-todo-input" onChange={e => setNewTodo(e.target.value)} value={newTodo} />
+						<div className="button" onClick={addTodo}>Create Task</div>
+					</div>
+				</div>
+			) : ''}
+		</div>
+	);
 }
 
-export default App
+export default App;
